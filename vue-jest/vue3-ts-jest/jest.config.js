8fcf5aee8aa22fb12f3df1b862cf0cbc92df7a55
arisemarcher <2543061999@qq.com>
2024-06-09 15:32:44 +0800

    feat: 代码文件目录整理

---
+++ vue-jest/vue3-ts-jest/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // ... 其他配置
+  testMatch: [
+    '**/tests/unit/**/*.(spec|test).(js|jsx|ts|tsx)',
+    '**/tests/unit/**/*.test.(js|jsx|ts|tsx)'
+  ],
+  // ... 其他配置
+};
---
+++ vue-jest/vue3-ts-jest/src/views/Home/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>This is an home page</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+console.log('home');
+</script>
+
+<style>
+</style>
